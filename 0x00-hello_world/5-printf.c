abf1ffd7d52f051e50ae99f4677b2604b97dab91
freshmohale <Phakitsomohale@gmail.com>
2022-09-08 06:27:17 -0700

    Task 3 to 6 updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include  <stdio.h>
+#include <stdio.h>
 
 /**
  * main - print the string in the put function
@@ -12,4 +12,3 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-^C
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -21,5 +21,5 @@ printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(li));
 printf("Size of a long long int: %ld bytes(s)\n", (unsigned long)sizeof(lli));
 printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(f));
 return(0);
+
 }
-^C
