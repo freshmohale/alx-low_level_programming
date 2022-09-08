48870c807815a88f7c99195ce38ec52f81c0681a
freshmohale <Phakitsomohale@gmail.com>
2022-09-08 07:45:15 -0700

    Task 8 updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
-#include <stdlib.h>
-
-
-
 /**
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   * Return: Always 1 (Success)
+ * Return: Always 1 (Success)
  *
- *    */
-
+ */
 int main(void)
-
 {
 
 		write(STDOUT_FILENO, "and that piece of art is useful
