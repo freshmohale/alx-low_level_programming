4c2946588a517b2b1fd213ce4b1e59475dedec01
freshmohale <Phakitsomohale@gmail.com>
2022-09-28 12:09:19 -0700

    Task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,15 @@
  *
  * Return: void
  */
-void _puts_recursion (char *s)
+void _puts_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
 		_putchar(s[0]);
-		_puts_recursion (s + 1);
+		_puts_recursion(s + 1);
 	}
 }
