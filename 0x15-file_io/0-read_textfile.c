f3153a52973d5a4f7df317baabc4804a7d5ace95
freshmohale <Phakitsomohale@gmail.com>
2022-10-31 07:37:19 -0700

    Read texfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,35 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
-* read_textfile - reads a text file and prints it to POISX stdout
-* @filename: A pointer to the filename 
-* @letters: The number of letters the
-* 		functionshould read and print
-*
-* Return: If the function fails or filename is NULL - 0.
-* 	O/w - the actual numberof btes the function can read and print.
-*/
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
-		return(0);
+	if (!filename)
+		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return(0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-	o = open(filename O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-	if (o == -1 || r == -1 || w == -1 || w!= r)
-	{
-		free(buffer);
-		return(0);
-	}
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	free(buffer);
-	close(o);
+	close(fd);
 
-	return(w);
+	free(buf);
+
+	return (nwr);
 }
